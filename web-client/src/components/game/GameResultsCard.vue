65d0291d10c65ba3732d9cd411a6fa72dee3faa5
Thomas FERREIRA <thomas_ferreira_93330@hotmail.fr>
2021-02-07 13:42:36 +0100

    To be updated

--- web-client/src/components/game/GameResultsCard.vue
+++ web-client/src/components/game/GameResultsCard.vue
@@ -11,54 +11,10 @@
       <div class="flex flex-initial justify-center">
         <v-card class="order-2 mb-14 mx-3 p-2" color="secondary darken-1" elevation="6">
           <img src="../../assets/first_place.png" width="150">
-          <h3 class="text-3xl text-center text-accent">900</h3>
-          <h4 class="text-2xl text-center">Toto</h4>
-        </v-card>
-        <v-card class="order-1 mt-14 mx-3 p-2" color="secondary darken-1" elevation="6">
-          <img src="../../assets/second_place.png" width="150">
-          <h3 class="text-3xl text-center text-accent">910</h3>
-          <h4 class="text-2xl text-center">Toto</h4>
-        </v-card>
-        <v-card class="order-3 mt-14 mx-3 p-2" color="secondary darken-1" elevation="6">
-          <img src="../../assets/third_place.png" width="150">
-          <h3 class="text-3xl text-center text-accent">850</h3>
-          <h4 class="text-2xl text-center">Toto</h4>
+          <h3 class="text-3xl text-center text-accent">{{this.playersScore[0].score}}</h3>
+          <h4 class="text-2xl text-center">{{this.playersScore[0].playerUsername}}</h4>
         </v-card>
       </div>
-      <ul class="flex flex-wrap justify-space-around my-8">
-        <li class="w-1/4">
-          <v-badge content="4" class="mx-auto w-3/4" overlap left bordered>
-            <v-card class="p-2" color="secondary darken-1" elevation="6">
-              <h3 class="text-3xl text-center text-accent">800</h3>
-              <h4 class="text-2xl text-center">Tata</h4>
-            </v-card>
-          </v-badge>
-        </li>
-        <li class="w-1/4">
-          <v-badge content="4" class="mx-auto w-3/4" overlap left bordered>
-            <v-card class="p-2" color="secondary darken-1" elevation="6">
-              <h3 class="text-3xl text-center text-accent">800</h3>
-              <h4 class="text-2xl text-center">Tutu</h4>
-            </v-card>
-          </v-badge>
-        </li>
-        <li class="w-1/4">
-          <v-badge content="4" class="mx-auto w-3/4" overlap left bordered>
-            <v-card class="p-2" color="secondary darken-1" elevation="6">
-              <h3 class="text-3xl text-center text-accent">800</h3>
-              <h4 class="text-2xl text-center">Titi</h4>
-            </v-card>
-          </v-badge>
-        </li>
-        <li class="w-1/4">
-          <v-badge content="4" class="mx-auto w-3/4" overlap left bordered>
-            <v-card class="p-2" color="secondary darken-1" elevation="6">
-              <h3 class="text-3xl text-center text-accent">800</h3>
-              <h4 class="text-2xl text-center">tyty</h4>
-            </v-card>
-          </v-badge>
-        </li>
-      </ul>
     </v-card>
   </section>
 </template>
